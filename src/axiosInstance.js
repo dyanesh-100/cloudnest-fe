6073776d50d52e71830fb4714f255d58c92c050d
Dyanesh D <you@example.com>
2025-05-28 13:28:38 +0530

    removed. unwanted console.logs

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,20 +1,23 @@
-import axios from 'axios'; 
+import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'https://cloudnest-be.vercel.app/api/v1',
     //http://localhost:3500/api/v1
     //https://cloudnest-be.vercel.app/api/v1
-    withCredentials: true, // ✅ Set at instance level
 });
+
+
+axiosInstance.defaults.withCredentials = true;
 
 axiosInstance.interceptors.request.use(
     (config) => {
-        config.withCredentials = true; // ✅ Also ensure per-request flag
+        
         return config;
     },
     (error) => {
         return Promise.reject(error);
     }
 );
+
 
 export default axiosInstance;
